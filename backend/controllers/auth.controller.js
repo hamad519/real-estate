a405294b4772fdb8f962890bce9b961f673a2a9c
Yasir Ali <sdr.yasir@gmail.com>
2024-08-01 05:10:05 -0700

    login feature implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -8,7 +8,7 @@ export default class AuthController{
         const user = req.body
         try {
             user.password = await bcrypt.hash(user.password, 10);
-            const user = await User.create(user)
+            await User.create(user)
            res.json({
             message:"user account has been created"
            })
@@ -18,7 +18,17 @@ export default class AuthController{
     }
 
     async login(req, res, next) {
-        const user = req.body
+        const body = req.body;
+
+        if(!body.email) return next(new Error('Provide the email'))
+        if(!body.password) return next(new Error('Provide the password')) 
+
+        const user = await User.findOne({email:body.email})
+        if(user === null) return next(new Error('this user is not registered'))
+
+        const isPasswordMatched = await bcrypt.compare(body.password, user.password)
+        if(!isPasswordMatched) return next(new Error('Invalid password'))
+
         try {
             res.json({
                 message:"login was called"
